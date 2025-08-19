a2b0977cd36dbb2bd4f3096350e86b403f454f6d
Vinícius Mergulhão <113155890+Vinimtt@users.noreply.github.com>
2025-08-19 23:37:29 +0000

    update: atividade mini bio

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,9 +1,15 @@
 export default function Home() {
   return (
     <div>
-      <h1>Ola turma!</h1>
-      <p>Teste de paragrafação</p>
-      <p>Teste novamente</p>
+      <h1>Vinícius Mergulhão Teti</h1>
+      <br />
+      <hr />
+      <br />
+      <img src="/assets/images/foto_vinicius.jpg" width={250} height={250} alt="Foto de Vinícius Mergulhão Teti"></img>
+      <hr />
+      <br />
+      <p>Sou estudante de Ciência da Computação e atualmente estou no 6º período.<br />Tenho interesse na área de Desenvolvimento BackEnd ou Análise de Dados,</p>
+      <p>Contante-me no <a href="https://www.linkedin.com/in/viníciusmergulhão" target="_blank">Linkedin!</a> </p>
     </div>
   );
 }
